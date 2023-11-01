7b54572f6f78ca15a27e2b1f90cca7ebedba9095
Deuri Vasquez <deuriib@outlook.com>
2023-11-01 21:57:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,80 @@
+function skillsMember() {
+    var skills = [
+        {
+            "name": "HTML",
+            "value": 90
+        },
+        {
+            "name": "CSS",
+            "value": 90
+        },
+        {
+            "name": "JavaScript",
+            "value": 80
+        },
+        {
+            "name": "jQuery",
+            "value": 80
+        },
+        {
+            "name": "PHP",
+            "value": 80
+        },
+        {
+            "name": "MySQL",
+            "value": 80
+        },
+        {
+            "name": "Photoshop",
+            "value": 80
+        },
+        {
+            "name": "Illustrator",
+            "value": 80
+        },
+        {
+            "name": "After Effects",
+            "value": 70
+        },
+        {
+            "name": "Premiere",
+            "value": 70
+        },
+        {
+            "name": "Cinema 4D",
+            "value": 70
+        },
+        {
+            "name": "WordPress",
+            "value": 60
+        },
+        {
+            "name": "React",
+            "value": 60
+        },
+        {
+            "name": "Angular",
+            "value": 60
+        },
+        {
+            "name": "Node.js",
+            "value": 60
+        },
+        {
+            "name": "MongoDB",
+            "value": 60
+        },
+        {
+            "name": "Express",
+            "value": 60
+        },
+        {
+            "name": "Sass",
+            "value": 60
+        },
+        {
+            "name": "Gulp",
+            "value": 60
+        },
+    ];
+}
